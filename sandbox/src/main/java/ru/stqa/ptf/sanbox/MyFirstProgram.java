35c7e655611c02504be8b30d3e3bcb42770ef7db
greiz596 <r.romadin2010@yandex.ru>
2022-04-24 13:27:25 +0300

    lesson 13

--- sandbox/src/main/java/ru/stqa/ptf/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sanbox/MyFirstProgram.java
@@ -1,9 +1,27 @@
 package ru.stqa.ptf.sanbox;
 
 public class MyFirstProgram {
+  public static void main(String[] args) {
+    hello("world!");
 
-	public static void main(String[] args) {
+    int x = 5;
+    System.out.println("Площадь квадрата со стороной " + x + " = " + area(x));
 
-System.out.println("Hello, world!");
-	}
+    int a = 4;
+    int b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b +" = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static int area(int l){
+    return l*l;
+  }
+
+  public static int area (int a, int b){
+    return a*b;
+  }
 }
